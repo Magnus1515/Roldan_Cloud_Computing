08647c107e8dfcec5b043f60c604df67b6746418
David Roldan <82829595+Magnus1515@users.noreply.github.com>
2023-05-01 11:40:49 -0700

    third commit

---
+++ action.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+const menuBtn =document.querySelector(".menu-btn");
+
+menu.addEventListener("click", () =>{
+    menu.classList.toggle("nav-toggle");
+});
+
